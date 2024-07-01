d34d106a7b34e3adc32778c404bc86172b23d167
Jurij Babicev <jurij.babicev@gmail.com>
2024-07-01 21:58:59 +0100

    New mountain time calculation added

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -13,7 +13,6 @@ function menuOption() {
 }
 
 // Dinamic new mountain task creation
-
 /* Task interation counter */
 var newTaskInter = 1;
 
@@ -22,6 +21,9 @@ function addTask() {
 
   const firstCheckup = document.getElementById("nm"+newTaskInter+"_deadline");
   const secondCheckup = document.getElementById("nm"+newTaskInter+"_name");
+
+  //Claculate diference
+
 
   /* Check or data entered                                  //
   // If not entered field will get red border (indianred)   //
@@ -48,6 +50,18 @@ function addTask() {
 
   if (first_permission && second_permission) {
 
+    // Date time difference calculation
+    const endDate = new Date(firstCheckup.value);
+    const startDate = Date.now();
+
+    // Calculate the time difference in milliseconds
+    const timeDifferenceMS = endDate - startDate;
+    // const diffTimeDate = new Date(timeDifferenceMS);
+
+    const timeDifferenceDays = Math.floor(timeDifferenceMS / (1000 * 60 * 60 * 24));
+    const timeDifferenceHours = Math.floor((timeDifferenceMS % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const timeDifferenceMins = Math.floor((timeDifferenceMS % (1000 * 60 * 60)) / (1000 * 60));
+
     //Task counter increment
     newTaskInter ++;
     // console.log("Task counter: " + newTaskInter);
@@ -71,7 +85,7 @@ function addTask() {
     timeLeft.setAttribute("type", "text");
     timeLeft.id ="nm"+newTaskInter+"_time_left";
     timeLeft.classList.add("time-left-field");
-    timeLeft.value = "JB Test"; /*test field*/
+    timeLeft.value = "";
     timeLeft.readOnly = true;
 
 
@@ -105,14 +119,23 @@ function addTask() {
     let parrent_box = document.getElementById("tasks-list");
     parrent_box.insertBefore(taskBox, parrent_box.firstChild);
 
+    //Date and time left till task deadline
+    document.getElementById("nm"+(newTaskInter-1)+"_time_left").value = timeDifferenceDays+"d "+timeDifferenceHours+"h "+timeDifferenceMins+"m"; /*test field*/
+
     // document.getElementById("tasks-list").insertBefore(taskBox);
     }
 }
 
 function deleteTask(target_id) {
-  alert("deleting" + target_id);
-  const delTask = document.getElementById("task_nr"+target_id);
-  delTask.remove();
+  let biggerId = target_id + 1;
+  // alert ("Delete ID: "+target_id+"  Next ID:"+biggerId+ "Max_ID: "+ newTaskInter);
+
+  if (biggerId > newTaskInter) {
+    // Action if can't delet element
+  } else {
+    const delTask = document.getElementById("task_nr"+target_id);
+    delTask.remove();
+  }
 }
 
 function newTaskCounterReset() {
