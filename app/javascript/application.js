ea03736d96fa886027d86333dd0160a28fcbb479
Vaida Kareivaite <157854257+VaidaKa@users.noreply.github.com>
2024-07-09 22:51:22 +0100

    circle progress bar added in index page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,4 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-// import "progress"  
-
-import { application } from "@hotwired/stimulus"
-import GoalsController from "controllers/goals_controller"
-application.register("goals", GoalsController)
+// import "progress"
